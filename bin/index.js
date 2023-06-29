8881d6bf1a926181fdf779b9ae5126c5b9c61abb
dmitriygrosh <dmitriy.grosh@gmail.com>
2023-06-29 17:58:28 +0300

    add version

--- bin/index.js
+++ bin/index.js
@@ -5,6 +5,11 @@ import { generate } from"./commands/generate.js";
 import { FILE_EXTENSION_CONST } from "./utils/const.js";
 
 program
+  .name('grosh-cli')
+  .description('CLI to manage folders in react appp')
+  .version('1.0.2');
+
+program
   .command('generate <path> <filename>')
   .description('generate directory with react files')
   .option(`--${FILE_EXTENSION_CONST.STYLE} <type>`, 'type for styles', FILE_EXTENSION_CONST.SCSS)
